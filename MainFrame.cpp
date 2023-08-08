21db54ef64d61872ed5acf07ea80db9d661607e8
DrDeveloperJ <Kamalmarauli@Icloud.com>
2023-08-08 18:08:50 +0100

    Added Heading Text to Main Window

--- MainFrame.cpp
+++ MainFrame.cpp
@@ -2,5 +2,16 @@
 
 MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title)
 {
+	CreateOptions();
+}
 
+void MainFrame::CreateOptions()
+{
+	wxFont headingFont(wxFontInfo(wxSize(0, 36)).Bold());
+	panel = new wxPanel(this);
+	headingText = new wxStaticText(panel, wxID_ANY, "Stock Management Page",
+		wxPoint(0, 22), wxSize(1000, -1), wxALIGN_CENTER_HORIZONTAL);
+
+	headingText->SetBackgroundColour(*wxBLUE);
+	headingText->SetFont(headingFont);
 }
--- MainFrame.h
+++ MainFrame.h
@@ -5,5 +5,14 @@ class MainFrame : public wxFrame
 {
 public:
 	MainFrame(const wxString& title);
+
+private:
+	void CreateOptions();
+
+	wxPanel* panel;
+	wxStaticText* headingText;
+	wxTextCtrl* textInput;
+	wxButton* createButton;
+	wxPanel* panel2;
 };
 
